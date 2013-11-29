c97ad136435c73294eafc1c6694e203612bcf3c9
Kuci <kuci24@gmail.com>
2013-11-29 16:34:54 +0100

    Add About screen to the settings

--- CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/SettingsActivity.java
+++ CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.frozendevs.cache.cleaner;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -12,17 +15,26 @@ public class SettingsActivity extends PreferenceActivity {
 
         addPreferencesFromResource(R.xml.settings);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        findPreference("licences").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                WebView webView = new WebView(getApplicationContext());
+                webView.loadUrl("file:///android_asset/html/licenses.html");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
+                AlertDialog dialog = new AlertDialog.Builder(SettingsActivity.this).create();
+                dialog.setTitle(R.string.open_source_licences);
+                dialog.setView(webView);
+                dialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(android.R.string.ok),
+                        new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show();
+
                 return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+            }
+        });
     }
 }
