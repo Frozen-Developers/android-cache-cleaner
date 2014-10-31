18caa522534a37cbf639a2f30d787bbd485e38a9
Kuci <kuci24@gmail.com>
2014-10-31 20:23:30 +0100

    Settings: Do not build subscreens with nested PreferenceScreen elements

--- CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/SettingsActivity.java
+++ CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/SettingsActivity.java
@@ -11,35 +11,42 @@ import com.frozendevs.cache.cleaner.R;
 
 public class SettingsActivity extends PreferenceActivity {
 
+    private final static String ACTION_SETTINGS_ABOUT = "com.frozendevs.cache.cleaner.activity.SETTINGS_ABOUT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.settings);
+        String action = getIntent().getAction();
+        if (action != null && action.equals(ACTION_SETTINGS_ABOUT)) {
+            addPreferencesFromResource(R.xml.settings_about);
 
-        String versionName;
-        try {
-            versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-        } catch (Exception e) {
-            versionName = getString(R.string.version_number_unknown);
+            String versionName;
+            try {
+                versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            } catch (Exception e) {
+                versionName = getString(R.string.version_number_unknown);
+            }
+            findPreference("version").setSummary(versionName);
+
+            findPreference("licences").setOnPreferenceClickListener(
+                    new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference preference) {
+                            WebView webView = new WebView(getApplicationContext());
+                            webView.loadUrl("file:///android_asset/html/licenses.html");
+
+                            AlertDialog dialog = new AlertDialog.Builder(SettingsActivity.this).create();
+                            dialog.setTitle(R.string.open_source_licences);
+                            dialog.setView(webView);
+                            dialog.show();
+
+                            return true;
+                        }
+                    });
+        } else {
+            addPreferencesFromResource(R.xml.settings);
         }
-        findPreference("version").setSummary(versionName);
-
-        findPreference("licences").setOnPreferenceClickListener(
-                new Preference.OnPreferenceClickListener() {
-                    @Override
-                    public boolean onPreferenceClick(Preference preference) {
-                        WebView webView = new WebView(getApplicationContext());
-                        webView.loadUrl("file:///android_asset/html/licenses.html");
-
-                        AlertDialog dialog = new AlertDialog.Builder(SettingsActivity.this).create();
-                        dialog.setTitle(R.string.open_source_licences);
-                        dialog.setView(webView);
-                        dialog.show();
-
-                        return true;
-                    }
-                });
     }
 
     @Override
