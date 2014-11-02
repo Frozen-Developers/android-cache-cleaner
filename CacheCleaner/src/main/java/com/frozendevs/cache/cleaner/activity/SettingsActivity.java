7f948f0cb217f3abbce0e2c4a4b06efe5d40d94b
Kuci <kuci24@gmail.com>
2014-11-02 22:33:31 +0100

    Settings: Add action bar using android.support.v7.widget.Toolbar
    This approach uses setContentView() for Toolbar injection.

--- CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/SettingsActivity.java
+++ CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/SettingsActivity.java
@@ -1,11 +1,16 @@
 package com.frozendevs.cache.cleaner.activity;
 
 import android.app.AlertDialog;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
 
 import com.frozendevs.cache.cleaner.R;
 
@@ -13,11 +18,14 @@ public class SettingsActivity extends PreferenceActivity {
 
     private final static String ACTION_SETTINGS_ABOUT = "com.frozendevs.cache.cleaner.activity.SETTINGS_ABOUT";
 
+    private Toolbar mActionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         String action = getIntent().getAction();
+
         if (action != null && action.equals(ACTION_SETTINGS_ABOUT)) {
             addPreferencesFromResource(R.xml.settings_about);
 
@@ -47,16 +55,34 @@ public class SettingsActivity extends PreferenceActivity {
         } else {
             addPreferencesFromResource(R.xml.settings);
         }
+
+        mActionBar.setTitle(getTitle());
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
+    public void setContentView(int layoutResID) {
+        ViewGroup contentView = (ViewGroup) LayoutInflater.from(this).inflate(layoutResID,
+                new LinearLayout(this), false);
 
-        return super.onOptionsItemSelected(item);
+        TypedArray typedArray = getTheme().obtainStyledAttributes(R.style.Theme_Application,
+                new int[]{R.attr.colorPrimary, R.attr.homeAsUpIndicator});
+
+        mActionBar = new Toolbar(this);
+        mActionBar.setBackgroundResource(typedArray.getResourceId(0,
+                R.color.primary_material_dark));
+        mActionBar.setNavigationIcon(typedArray.getResourceId(1,
+                R.drawable.abc_ic_ab_back_mtrl_am_alpha));
+        mActionBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        typedArray.recycle();
+
+        contentView.addView(mActionBar, 0);
+
+        getWindow().setContentView(contentView);
     }
 }
