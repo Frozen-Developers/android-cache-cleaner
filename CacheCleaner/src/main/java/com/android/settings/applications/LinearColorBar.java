e8422198d54aefc8353fdf4adc5cc90b97899089
Kuci <kuci24@gmail.com>
2014-11-07 11:20:16 +0100

    Update LinearColorBar to the latest version

--- CacheCleaner/src/main/java/com/android/settings/applications/LinearColorBar.java
+++ CacheCleaner/src/main/java/com/android/settings/applications/LinearColorBar.java
@@ -16,9 +16,9 @@ import android.view.MotionEvent;
 import android.widget.LinearLayout;
 
 public class LinearColorBar extends LinearLayout {
-    static final int LEFT_COLOR = 0xff0099cc;
-    static final int MIDDLE_COLOR = 0xff0099cc;
-    static final int RIGHT_COLOR = 0xff888888;
+    static final int LEFT_COLOR = 0xff009688;
+    static final int MIDDLE_COLOR = 0xff009688;
+    static final int RIGHT_COLOR = 0xffced7db;
     static final int GRAY_COLOR = 0xff555555;
     static final int WHITE_COLOR = 0xffffffff;
 
@@ -70,7 +70,7 @@ public class LinearColorBar extends LinearLayout {
                 ? 2 : 1;
         mEdgeGradientPaint.setStrokeWidth(mLineWidth);
         mEdgeGradientPaint.setAntiAlias(true);
-        
+
     }
 
     public void setOnRegionTappedListener(OnRegionTappedListener listener) {
