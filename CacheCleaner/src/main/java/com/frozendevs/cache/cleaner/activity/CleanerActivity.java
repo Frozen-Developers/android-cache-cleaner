5a0cc63ec8c58dd71935b6b1ddfc43ec240cd5c1
Kuci <kuci24@gmail.com>
2015-05-04 21:45:19 +0200

    CleanerActivity: Extend AppCompatActivity instead of deprecated ActionBarActivity

--- CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/CleanerActivity.java
+++ CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/CleanerActivity.java
@@ -1,11 +1,11 @@
 package com.frozendevs.cache.cleaner.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.frozendevs.cache.cleaner.R;
 
-public class CleanerActivity extends ActionBarActivity {
+public class CleanerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
