64ece17f3c3bb481a4a813f611947eac80140c03
Kuci <kuci24@gmail.com>
2015-09-19 14:46:15 +0200

    Settings: Merge AboutFragment into SettingsFragment

--- CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/SettingsActivity.java
+++ CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/activity/SettingsActivity.java
@@ -5,29 +5,21 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-import com.frozendevs.cache.cleaner.fragment.AboutFragment;
 import com.frozendevs.cache.cleaner.fragment.SettingsFragment;
 
 public class SettingsActivity extends AppCompatActivity {
-
-    private final static String ACTION_SETTINGS_ABOUT = "com.frozendevs.cache.cleaner.SETTINGS_ABOUT";
+    public final static String ARGUMENT_ACTION = "action";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final Fragment fragment;
-
-        String action = getIntent().getAction();
-
-        if (action != null && action.equals(ACTION_SETTINGS_ABOUT)) {
-            fragment = new AboutFragment();
-        } else {
-            fragment = new SettingsFragment();
-        }
+        final Bundle bundle = new Bundle();
+        bundle.putString(ARGUMENT_ACTION, getIntent().getAction());
 
         getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, fragment)
+                .replace(android.R.id.content, Fragment.instantiate(this,
+                        SettingsFragment.class.getName(), bundle))
                 .commit();
     }
 
--- CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/fragment/SettingsFragment.java
+++ CacheCleaner/src/main/java/com/frozendevs/cache/cleaner/fragment/SettingsFragment.java
@@ -1,13 +1,54 @@
 package com.frozendevs.cache.cleaner.fragment;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.webkit.WebView;
 
 import com.frozendevs.cache.cleaner.R;
+import com.frozendevs.cache.cleaner.activity.SettingsActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+    private final static String ACTION_SETTINGS_ABOUT = "com.frozendevs.cache.cleaner.SETTINGS_ABOUT";
+
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
-        addPreferencesFromResource(R.xml.settings);
+        final String action = (String) getArguments().get(SettingsActivity.ARGUMENT_ACTION);
+
+        if (action != null && action.equals(ACTION_SETTINGS_ABOUT)) {
+            addPreferencesFromResource(R.xml.settings_about);
+
+            String versionName;
+            try {
+                versionName = getContext().getPackageManager().getPackageInfo(
+                        getContext().getPackageName(), 0).versionName;
+            } catch (Exception e) {
+                versionName = getString(R.string.version_number_unknown);
+            }
+            findPreference("version").setSummary(versionName);
+
+            findPreference("licences").setOnPreferenceClickListener(
+                    new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference preference) {
+                            WebView webView = new WebView(getContext());
+                            webView.loadUrl("file:///android_asset/html/licenses.html");
+
+                            AlertDialog dialog = new AlertDialog.Builder(getContext()).create();
+                            dialog.setTitle(R.string.open_source_licences);
+                            dialog.setView(webView);
+                            dialog.show();
+
+                            return true;
+                        }
+                    });
+
+            getActivity().setTitle(R.string.about_app);
+        } else {
+            addPreferencesFromResource(R.xml.settings);
+
+            getActivity().setTitle(R.string.settings);
+        }
     }
 }
